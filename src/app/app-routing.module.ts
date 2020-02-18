0f7fbd7d8350f840a843c0a4c871af84c7f7af20
Jos Vermoesen <info@rv.be>
2020-02-18 13:28:53 +0100

    components and routingtest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/navbar/home/home.component';
+import { AboutComponent } from './components/navbar/about/about.component';
+import { BlogComponent } from './components/navbar/blog/blog.component';
+import { ContactComponent } from './components/navbar/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { ContactComponent } from './components/navbar/contact/contact.component';
+import { BlogComponent } from './components/navbar/blog/blog.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -16,16 +18,27 @@ import { ModalModule, BsModalRef } from 'ngx-bootstrap/modal';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { LanguageService } from './_services/language.service';
+import { HomeComponent } from './components/navbar/home/home.component';
+import { AboutComponent } from './components/navbar/about/about.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LanguageComponent } from './components/navbar/language/language.component';
+
+import { LanguageService } from './_services/language.service';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
 }
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, LanguageComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent,
+    LanguageComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
